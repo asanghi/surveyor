729c6e20dff03c3d07783bb3933f20966c5dbc2a
Aditya Sanghi <asanghi@me.com>
2010-09-18 02:07:26 +0530

    aligning with master branch changes

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
 # Init hook code here
-require 'surveyor/acts_as_response'
+require 'surveyor'
--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -1,3 +1,5 @@
+require 'surveyor/common.rb'
+require 'surveyor/acts_as_response.rb'
 module Surveyor
   require 'surveyor/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
