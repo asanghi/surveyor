1c0ca5f0dd6eebda89094b842be6de3aa55fb88a
Aditya Sanghi <asanghi@me.com>
2010-09-18 02:42:47 +0530

    more rails3 related changes

--- lib/surveyor.rb
+++ lib/surveyor.rb
@@ -1,5 +1,6 @@
 require 'surveyor/common.rb'
 require 'surveyor/acts_as_response.rb'
+require 'surveyor/surveyor_controller_methods.rb'
 module Surveyor
   require 'surveyor/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
